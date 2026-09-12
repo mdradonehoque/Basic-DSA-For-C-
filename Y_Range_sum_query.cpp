73673ed4057dfb6fa9dccc1391c0d2684a9deca7
Md. Radone Hoque <mdradonehoque@gmail.com>
2026-09-12 23:03:39 +0600

    Update Y_Range_sum_query.cpp

--- Y_Range_sum_query.cpp
+++ Y_Range_sum_query.cpp
@@ -4,19 +4,33 @@ int main()
 {
     int n, q;
     cin >> n >> q;
-    vector<int> v(n + 1);
+    vector<long long int> v(n + 1);
     for (int i = 1; i <= n; i++)
     {
         cin >> v[i];
     }
+    vector<long long int> pre(n + 1);
+    pre[1] = v[1];
+    for (int i = 2; i <= n; i++)
+    {
+        pre[i] = pre[i - 1] + v[i];
+    }
+    // for (int i = 1; i <= n; i++) prefix sum test
+    //{
+    // cout << pre[i] << " ";
+    //}
     while (q--)
     {
         int l, r;
         cin >> l >> r;
-        int sum = 0;
-        for (int i = l; i <= r; i++)
+        long long int sum;
+        if (l == 1)
         {
-            sum += v[i];
+            sum = pre[r];
+        }
+        else
+        {
+            sum = pre[r] - pre[l - 1];
         }
         cout << sum << endl;
     }
